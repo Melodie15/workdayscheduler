c983c81526fe19281f15958158dd93f36055e515
frobbins <robbinsfatim@gmail.com>
2020-11-02 22:53:27 -0700

    Added function to change rows color based on time

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+//Today's date
 $("#today").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//Check colors on past, present and future rows
+var date = new Date();
+var hours = date.getHours();
+function confirmDate() {
+    for (time = 9; time < 23; time ++) {
+        var timeRow = $("#" + time);
+        console.log($("#" + time))
+
+        if(time == hours) {
+            timeRow.addClass("present")
+        } else if (time < hours) {
+            timeRow.addClass("past")
+        } else {
+            timeRow.addClass("future")
+        }
+        $("#" + time).val(localStorage.getItem("" + time));
+    }
+}
+confirmDate();
